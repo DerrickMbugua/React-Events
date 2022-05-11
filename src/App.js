73d911706ce8d64577a0dc90fd05d11d5bd7d1c5
Derrick Mwema <derrickmbugua50@gmail.com>
2022-05-12 02:10:49 +0300

    feat: events

--- src/App.js
+++ src/App.js
@@ -1,5 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+function Welcome(props){
+  return(
+    <h1>Hello, {props.name}</h1>
+  )
+}
+
+class Details extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={name: 'Dex'}
+  }
+  render(){
+    return(
+      <div>
+        <h2>My name is {this.state.name}</h2>
+      </div>
+    )
+  }
+}
+
+class Toggle extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
 
 function App() {
   return (
@@ -18,6 +63,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <Welcome name='Saru' />
+        <Details />
+        <Toggle />
+      </body>
     </div>
   );
 }
