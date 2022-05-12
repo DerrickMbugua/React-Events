d8e65f70b33cd039288357da8ac98ac5a7840cd7
Derrick Mwema <derrickmbugua50@gmail.com>
2022-05-12 09:09:18 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function Welcome(props){
-  return(
+function Welcome(props) {
+  return (
     <h1>Hello, {props.name}</h1>
   )
 }
 
-class Details extends React.Component{
-  constructor(props){
+class Details extends React.Component {
+  constructor(props) {
     super(props)
-    this.state ={name: 'Dex'}
+    this.state = { name: 'Dex' }
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
         <h2>My name is {this.state.name}</h2>
       </div>
@@ -22,10 +22,10 @@ class Details extends React.Component{
   }
 }
 
-class Toggle extends React.Component{
+class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {isToggleOn: true};
+    this.state = { isToggleOn: true };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
@@ -45,6 +45,100 @@ class Toggle extends React.Component{
     );
   }
 }
+
+function UserGreetings(props) {
+  return (
+    <h1>Welcome Back</h1>
+  )
+}
+
+function GuestGreetings(props) {
+  return (
+    <h1>Welcome Guest</h1>
+  )
+}
+
+function Greetings(props) {
+  const isLogged = props.isLogged
+  if (isLogged) {
+    return <UserGreetings />
+  } else {
+    return <GuestGreetings />
+  }
+}
+
+function LoginButton(props) {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        Login
+      </button>
+    </div>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <div>
+      <button onClick={props.onClick}>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+    this.state = { isLogged: false }
+  }
+
+  handleLoginClick() {
+    this.setState({ isLogged: true })
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLogged: false })
+  }
+
+  render() {
+    const isLogged = this.state.isLogged
+    let button
+
+    if (isLogged) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+
+    return (
+      <div>
+        <Greetings isLogged={this.state.isLogged} />
+        {button}
+        {isLogged ? <LogoutButton onClick={this.handleLogoutClick} />
+          : <LoginButton onClick={this.handleLoginClick} />}
+      </div>
+    )
+  }
+}
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['D', 'E', 'R', 'O']
 
 function App() {
   return (
@@ -67,6 +161,9 @@ function App() {
         <Welcome name='Saru' />
         <Details />
         <Toggle />
+        <Greetings isLogged={true} />
+        <LoginControl />
+        <Mailbox unreadMessages={messages} />
       </body>
     </div>
   );
